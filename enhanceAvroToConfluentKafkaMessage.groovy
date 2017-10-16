b0929664618be1a5bdb600c53d33fce4654aeb7c
Vinson Zhang <vinson.zhang@gmail.com>
2017-10-16 13:56:38 -0400

    Update enhanceAvroToConfluentKafkaMessage.groovy

--- enhanceAvroToConfluentKafkaMessage.groovy
+++ enhanceAvroToConfluentKafkaMessage.groovy
@@ -1,6 +1,7 @@
-
-
-
+/**
+** Converts avro binary to confluent kafka message 
+*   
+*/
 def flowFileList = session.get(100)
 if (flowFileList.isEmpty()) return
 
@@ -13,22 +14,21 @@ byte MAGIC_BYTE = 0x0;
 int idSize = 4;
 
 flowFileList.each { flowFile ->
-       try{
-        
+       try {
            String schemaIdStr = flowFile.getAttribute("confluent.schema.id");
            if ( schemaIdStr == null || schemaIdStr.trim() == "" ) {
              throw new RuntimeException ("No schema id found, attribute 'confluent.schema.id' not populated");
            }
-          int schemaId = Integer.valueOf(schemaIdStr)
+           int schemaId = Integer.valueOf(schemaIdStr)
 
-            def newFlowFile = session.write(flowFile,   { InputStream inputStream, OutputStream outputStream ->
+           def newFlowFile = session.write(flowFile,   { InputStream inputStream, OutputStream outputStream ->
                  outputStream.write(MAGIC_BYTE);
                  outputStream.write(ByteBuffer.allocate(idSize).putInt(schemaId).array());
                  org.apache.commons.io.IOUtils.copy(inputStream,outputStream,100*1024);
                 } as StreamCallback
-             );
-            session.transfer(newFlowFile, REL_SUCCESS);
-            session.remove(flowFile)
+           );
+           session.transfer(newFlowFile, REL_SUCCESS);
+           session.remove(flowFile)
        } catch (Exception ex){
            log.error("Unable to convert avro to confluent kafka message.", ex)
            session.transfer(flowFile, REL_FAILURE);
